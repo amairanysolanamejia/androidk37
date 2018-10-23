e429e85abec5c569b8d3d04fdc9de087e02274cc
RodrigoFrancisco <rodrigofranciscopablo@gmail.com>
2018-10-23 16:29:58 -0500

    lo visto en la clase 02

---
+++ src/6_GUI/ejemplos/GUIS/app/src/main/java/com/example/rho/guis/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rho.guis
+
+import android.support.v7.app.AppCompatActivity
+// AppCompatActivity y Activity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear)
+    }
+}
